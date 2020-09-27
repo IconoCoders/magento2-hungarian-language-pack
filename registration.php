fb39819ec4405aae1ce7b12d659160d50d21d3a2
kovax <git@progweb.hu>
2020-09-27 18:41:38 +0200

    Create registration.php

---
+++ registration.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ *                     Iconocoders_Hungarian package
+ *                     NOTICE OF LICENSE
+ *
+ *                     This source file is subject to the MIT License
+ *                     that is bundled with this package in the file LICENSE.txt.
+ *                     It is also available through the world-wide-web at this URL:
+ *                     ttps://icoders.co/LICENSE.txt
+ *
+ *                     @category  Iconocoders
+ *                     @package   Iconocoders_Hungarian
+ *                     @copyright Copyright (c) 2020
+ *                     @license   https://icoders.co
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
+    'iconocoders_hu_hu',
+    __DIR__
+);
